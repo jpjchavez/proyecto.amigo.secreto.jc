b5ec3ec6357ca753212252b32f420407296d6281
jpjchavez <jpjchavez@gmail.com>
2025-08-25 01:48:42 -0500

    Carga entregable1

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# proyecto.amigo.secreto.jc
+# Proyecto Amigo Secreto
+
+Este es un proyecto simple en **JavaScript** que permite:
+
+- Agregar amigos a una lista.
+- Mostrar los amigos en pantalla.
+- Realizar un sorteo aleatorio para elegir un amigo.
+
+##  ¿Cómo usarlo?
+
+1. Escribe un nombre en la caja de texto.  
+2. Haz clic en **Agregar** para añadirlo a la lista.  
+3. Cuando tengas todos los nombres, presiona **Sortear Amigo** para elegir uno al azar.  
+![imagen](./ejemplo.png)
+
+## 📂 Tecnologías usadas
+
+- **HTML5** para la estructura.
+- **CSS3** para estilos básicos.
+- **JavaScript** para la lógica del listado y sorteo.
+
+Desarrollado por Jean Chavez
--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+let nombre;
+
+function agregarAmigo() {
+    //valor ingresado "amigo" como variable "nombre"
+    nombre=document.getElementById('amigo').value;
+    if (nombre=='') {
+        alert ("Por favor, ingrese nombre");
+    } else {
+        agregarLista();
+        limpiarCaja();
+        enlistadoAmigos();
+    }
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value='';
+}
+
+function agregarLista() {
+    amigos.push(nombre);
+}
+
+function enlistadoAmigos (){
+    //enlista a mostrar
+    let lista=document.getElementById('listaAmigos');
+    lista.innerHTML=""; //limpia la lista
+    for (let i=0;i<amigos.length;i++) {
+        let li =document.createElement("li");
+        li.textContent=amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos==[]){
+        alert ("La lista de amigos esta vacía");
+    } else {
+        let numeroMaximo=amigos.length;
+        let numeroGenerado = Math.floor(Math.random()*numeroMaximo)
+        //mostrar resultado
+        let nombreSorteado=document.getElementById('resultado');
+        nombreSorteado.innerHTML=amigos[numeroGenerado];
+
+    }
+}
